ed6e0f367370cf33975fdf5a3e190528391dba9f
Kaczo24 <554554554+Kaczo24@users.noreply.github.com>
2021-02-04 15:12:08 +0100

    PSI - zad 3 v 2

--- PSI_ZAD/zad_3/main.js
+++ PSI_ZAD/zad_3/main.js
@@ -1,6 +1,6 @@
 //document.cookie.split("; ").find(x => x.startsWith("zad_3-settings")).substr(14);
 //document.cookie = "zad_3-settings=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-
+let settingsCookie = "zad_3-settings";
 let settings = {
     "BgColorHead": "#5A5A5A",
     "FontColorHead": "#c4c4cb",
@@ -8,31 +8,44 @@ let settings = {
     "FontColorText": "#000000"
 };
 
+document.body.onload = () => {
+    try {
+         settings = JSON.parse(document.cookie.split("; ").find(x => x.startsWith(settingsCookie)).substr(14));
+    } catch (e) {
+        return;
+    }
+    for(let x of document.getElementsByClassName("HEADER"))
+        x.style.backgroundColor = settings.BgColorHead;
+    for(let x of document.getElementsByClassName("HEADER"))
+        x.style.color = settings.FontColorHead;
+    document.body.style.backgroundColor = settings.BgColorText;
+    document.body.style.color = settings.FontColorText;
+}
 
 
 function ChangeBgColorHead(event) {
-    for(let x of document.getElementsByClassName("HEADER"))
-        x.style.backgroundColor = event.currentTarget.value;
     settings.BgColorHead = event.currentTarget.value;
-    document.cookie = "zad_3-settings=" + JSON.stringify(settings);
+    for(let x of document.getElementsByClassName("HEADER"))
+        x.style.backgroundColor = settings.BgColorHead;
+    document.cookie = settingsCookie + "=" + JSON.stringify(settings);
 }
 
 function ChangeFontColorHead(event) {
-    for(let x of document.getElementsByClassName("HEADER"))
-        x.style.color = event.currentTarget.value;
     settings.FontColorHead = event.currentTarget.value;
-    document.cookie  = "zad_3-settings=" + JSON.stringify(settings);
+    for(let x of document.getElementsByClassName("HEADER"))
+        x.style.color = settings.FontColorHead;
+    document.cookie  = settingsCookie + "=" + JSON.stringify(settings);
 }
 
 
 function ChangeBgColorText(event) {
-    document.body.style.backgroundColor = event.currentTarget.value;
     settings.BgColorText = event.currentTarget.value;
-    document.cookie  = "zad_3-settings=" + JSON.stringify(settings);
+    document.body.style.backgroundColor = settings.BgColorText;
+    document.cookie  = settingsCookie + "=" + JSON.stringify(settings);
 }
 
 function ChangeFontColorText(event) {
-    document.body.style.color = event.currentTarget.value;
     settings.FontColorText = event.currentTarget.value;
-    document.cookie  = "zad_3-settings=" + JSON.stringify(settings);
+    document.body.style.color = settings.FontColorText;
+    document.cookie  = settingsCookie + "=" + JSON.stringify(settings);
 }
